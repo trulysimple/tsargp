2a9c31b70d514b8582853672ec8f3fb2baf27735
Diego Sogari <diego.sogari@gmail.com>
2024-02-18 23:31:28 -0300

    Update docs (v0.1.5)

--- docs/components/demo.tsx
+++ docs/components/demo.tsx
@@ -12,7 +12,7 @@ import 'xterm/css/xterm.css';
 // @ts-expect-error does not export types
 import options from 'tsargp/demo';
 // override version because there's no package.json file in the browser
-options.version.version = '0.1.52';
+options.version.version = '0.1.57';
 
 //--------------------------------------------------------------------------------------------------
 // Types
