e124541e6d92430fa2f79e4e7f87b1a62147ef6b
Diego Sogari <diego.sogari@gmail.com>
2024-03-02 15:57:26 -0300

    Update documentation

--- docs/components/demo.tsx
+++ docs/components/demo.tsx
@@ -7,7 +7,7 @@ import { type Props, Command } from './command';
 // @ts-expect-error since tsargp demo does not export types
 import { demo as options } from 'tsargp/examples';
 // override version because there's no package.json file in the browser
-options.version.version = '0.1.90';
+options.version.version = '0.1.93';
 
 //--------------------------------------------------------------------------------------------------
 // Classes
