6e625e65440657a1fe4d7eeacd7422dd9dad3599
Diego Sogari <diego.sogari@gmail.com>
2024-03-10 15:04:47 -0300

    Override code owners for added changesets

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,7 +4,7 @@ export default {
   'package.json': (paths) => paths.map((path) => `publint --strict '${dirname(path)}'`),
   '*.{js,ts,jsx,tsx,mdx}': (paths) => `eslint ${paths.map((path) => `'${path}'`).join(' ')}`,
   '*': (paths) => [
-    `cspell lint ${paths.map((path) => `'${path}'`).join(' ')}`,
+    `cspell --no-must-find-files ${paths.map((path) => `'${path}'`).join(' ')}`,
     `prettier --write ${paths.map((path) => `'${path}'`).join(' ')}`,
   ],
 };
